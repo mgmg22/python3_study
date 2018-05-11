662bfc4b919b549d93c0f64cab417834cac79a17
sxs <shenxs@fshows.com>
2018-05-11 09:56:37 +0800

    添加钉钉机器人

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # jisilu_spider
 Python爬虫项目
+
 1.爬取集思录帖子
-2.tower.im 检查日报
+
+2.tower.im 检查日报,钉钉机器人提醒
+
--- tower_all.py
+++ tower_all.py
@@ -4,8 +4,10 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import json
 
 projectUrl = 'https://tower.im/projects/29fa2bc07a984a84aed9e3593d507c25/'
+ding_url = "https://oapi.dingtalk.com/robot/send?access_token=1fc402abdd2b7dec04921423b45415687f19dd817d4fdae699ca703e855745d1"
 plan_home_url = [''] * 6
 flag = [0] * 6
 user = [
@@ -16,6 +18,17 @@ user = [
     '曹世鑫',
     '但彬',
 ]
+
+user_mobile = [
+    '15757179463',
+    '18296120635',
+    '17607185665',
+    '17605819508',
+    '15036142572',
+    '15820798016',
+]
+
+ding_mobile = [''] * 6
 
 daily_url = [
     'https://tower.im/projects/122a4eafcf1643f6bec2bba9d776fc7f/todos/a5fdf6339f7c46a78ef1f3c0b6d05932/',
@@ -34,6 +47,12 @@ urlPos = [
     10,
     6,
 ]
+
+ding_header = {
+    "Content-Type": "application/json",
+    "charset": "utf-8"
+}
+
 headers = {
     'Host': 'tower.im',
     'Connection': 'keep-alive',
@@ -45,6 +64,7 @@ headers = {
     'Accept-Language': 'zh-CN,zh;q=0.9',
     'Cookie': 'intercom-lou-vgeb94xf=1; intercom-id-vgeb94xf=546b02a0-1fc5-4f33-ba45-7393cb139cc6; _ga=GA1.2.2059481265.1510972641; intercom-lou-xbtsuf77=1; _gid=GA1.2.109886700.1525678234; _tower2_session=512d4e4dabfcd4e5c00fd8d430e974a3; _gat=1; _gat_teamTracker=1; intercom-session-xbtsuf77=eXRZeHpadkdMNkF0ekY5UnMvLytONU9wTHZjOUJhaGlkK1dqUXFmYXdRVXQ3M3AzTFVVSXVsd21SZkdoZ1lVRC0tTU1EZFplRVh1ZXhlOW43TDdLZnQrZz09--328d5d9f8a9a12659000a7efd238d4164448d7b8; remember_token=26bcc67f-d848-457a-a9da-11025384b70c'
 }
+today_result = ""
 
 
 # 获取每个人的项目计划url
@@ -75,7 +95,9 @@ def get_plan_item():
                     break
 
 
+# 检查日报
 def check_daily():
+    global today_result
     print(today + "日报提醒")
     for pos in range(0, len(daily_url)):
         data = requests.get(daily_url[pos], headers=headers)
@@ -86,14 +108,35 @@ def check_daily():
             if today in item.get('title'):
                 daily = 0
         if daily == 1:
-            print(user[pos] + "日报还没写")
+            ding_mobile[pos] = user_mobile[pos]
+            today_result += user[pos] + "日报还没写" + "\n"
 
 
 # 输出检查结果
 def get_check_result():
+    global today_result
     for index in range(len(flag)):
         if flag[index] == 0:
-            print(user[index] + '今日项目规划还未写')
+            ding_mobile[index] = user_mobile[index]
+            today_result += user[index] + '今日项目规划还未写' + "\n"
+
+
+# 同步钉钉机器人
+def send_ding():
+    if today_result == '':
+        return
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": today + "日报提醒" + "\n" + today_result
+        },
+        "at": {
+            "atMobiles": ding_mobile
+        }
+    }
+    send_data = json.dumps(data)
+    print(ding_mobile)
+    req = requests.post(ding_url, data=send_data, headers=ding_header)
 
 
 if __name__ == '__main__':
@@ -103,3 +146,4 @@ if __name__ == '__main__':
     get_plan_url()
     get_plan_item()
     get_check_result()
+    send_ding()
