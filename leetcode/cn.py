89eb68e3a7587b365632fe79fde8537dbeda5e22
sxs <shenxs@fshows.com>
2018-11-17 17:00:51 +0800

    最长回文子串、单词压缩编码

--- leetcode/cn.py
+++ leetcode/cn.py
@@ -71,7 +71,67 @@ class Solution:
         # print(raw_max, line_max)
         return result
 
+    """
+       5. 最长回文子串—Manacher算法
+       """
+
+    def longestPalindrome(self, s):
+        arr = ["#"]
+        for item in list(s):
+            arr.append(item)
+            arr.append("#")
+        print(arr)
+        result = ""
+        max_result = ""
+        size = arr.__len__()
+        for i in range(1, size - 1):
+            step = 0
+            middle = arr[i]
+            while middle != result:
+                step += 1
+                if i - step < 0 or i + step == size:
+                    break
+                if step != 1:
+                    middle = result
+                low = arr[i + step]
+                high = arr[i - step]
+                if low == high:
+                    result = low + middle + high
+                else:
+                    break
+                print(step, result)
+            if len(result) > len(max_result):
+                max_result = result
+            if i + step == size:
+                print(i + step, "遍历结束")
+                break
+        return max_result.replace("#", "")
+
+    """
+         820. 单词的压缩编码
+         """
+
+    def minimumLengthEncoding(self, words):
+        list1 = sorted(list(words), key=lambda i: len(i), reverse=True)
+        print(list1)
+        str = ""
+        arr = []
+        for item in list1:
+            # print(str.find(item))
+            pos = str.find(item)
+            if pos < 0:
+                arr.append(len(str))
+                str = str + item + "#"
+            else:
+                arr.append(pos)
+        print(arr)
+        print(str)
+        return len(str)
+
 
 if __name__ == '__main__':
+    x = Solution
     # print(Solution.findPeakElement("", [1, 2, 3, 1]))
-    print(Solution.maxIncreaseKeepingSkyline("", [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]))
+    # print(Solution.maxIncreaseKeepingSkyline("", [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]))
+    # print(x.longestPalindrome("", "cbbd"))
+    print(x.minimumLengthEncoding("", ["time", "me", "bell"]))
