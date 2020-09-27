edc8cc869a5cdf56058dae9f2656e6c106722d2f
shenxiaoshun <mgmg22>
2020-09-27 15:13:29 +0800

    optimize:update readme

--- activituy_regx.py
+++ activituy_regx.py
@@ -3,11 +3,12 @@
 # @File  : http_unit.py
 # @Author: sxs
 # @Date  : 2020/1/18
-# @Desc  : 读取项目中的Activity的名称
+# @Desc  : 读取项目中的Activity的名称,需要安装lxml库
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-    file = open("../AndroidManifest.xml", encoding='utf-8').read()
+    file = open("./AndroidManifest.xml", encoding='utf-8').read()
     soup = BeautifulSoup(file, features='xml')
+    print(f"共有{len(soup.application.find_all('activity'))}个Activity")
     for item in soup.application.find_all('activity'):
         print(item.get("android:name"), item.get("android:label"))
