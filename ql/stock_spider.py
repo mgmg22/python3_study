8aae3184e311f94354a7eb9eedfec8188d3f942e
mgmg22 <d33hcx@qq.com>
2024-03-27 12:20:31 +0800

    添加etf

--- ql/stock_spider.py
+++ ql/stock_spider.py
@@ -17,9 +17,7 @@ indexFilters = {
     '北证50',
 }
 
-# TODO
-etf_url = 'https://www.jisilu.cn/data/etf/etf_list/?___jsl=LST___t=1710853843396&rp=25&page=1'
-ETFFilters = {
+fundFilters = {
     # 512690	酒ETF
     # 512760	芯片ETF
     '酒ETF',
@@ -30,19 +28,35 @@ notifyData = []
 
 
 # 指数涨幅统计
-def get_stock_index():
+def get_index_increase():
     list_url = 'https://www.jisilu.cn/data/idx_performance/list/?___jsl=LST___t=1710685332681'
     resp = requests.post(list_url)
     for row in resp.json()['rows']:
         if row['cell']['index_nm'] in indexFilters:
-            stock = {'name': row['cell']['index_nm'],
-                     'increase': row['cell']['increase_rt'],
-                     }
+            stock = {
+                'id': row['id'],
+                'name': row['cell']['index_nm'],
+                'increase': row['cell']['increase_rt'],
+            }
+            notifyData.append(stock)
+
+
+# etf涨幅统计
+def get_fund_increase():
+    fund_url = 'https://www.jisilu.cn/data/etf/etf_list/?___jsl=LST___t=1710853843396&rp=25&page=1'
+    resp = requests.post(fund_url)
+    for row in resp.json()['rows']:
+        if row['cell']['fund_nm'] in fundFilters:
+            stock = {
+                'id': row['id'],
+                'name': row['cell']['fund_nm'],
+                'increase': row['cell']['increase_rt'],
+            }
             notifyData.append(stock)
 
 
 #  专门获取医药生物（801150.SL）的数据
-def add_SW_increase():
+def add_sw_increase():
     sw_url = 'https://www.swsresearch.com/institute-sw/api/index_publish/details/index_spread/?swindexcode=801150'
     headers = {
         'accept': "*/*",
@@ -50,31 +64,34 @@ def add_SW_increase():
     }
     resp = requests.get(sw_url, headers=headers)
     data = resp.json()['data'][0]
-    lastDay = float(data['l3'])
+    last_day = float(data['l3'])
     now = float(data['l8'])
-    swData = {'name': "医药生物",
-              'increase': str(round(((now - lastDay) / lastDay) * 100, 2)),
-              }
+    swData = {
+        'id': '801150',
+        'name': "医药生物",
+        'increase': str(round(((now - last_day) / last_day) * 100, 2)),
+    }
     notifyData.append(swData)
 
 
 def notify_with_markdown():
-    content = '''# 指数涨幅统计
-| 名称 | 本日涨幅 |
-|:--------|--------:|
+    content = '''# 今日涨幅统计
+| 代码 | 名称 | 涨幅 |
+|:--------|:--------|--------:|
 '''
     for item in notifyData:
-        content += f'| {item["name"]} | {item["increase"]} %|\n'
+        content += f'| {item["id"]} | {item["name"]} | {item["increase"]} %|\n'
     notify.serverJMy(generate_title(), content)
-    # with open("Test.md", 'w') as f:
-    #     f.write(content)
+    with open("Test.md", 'w') as f:
+        f.write(content)
 
 
 def generate_title() -> str:
-    return "今日" + notifyData[0]["name"] + "涨幅为:" + notifyData[0]["increase"] + "%"
+    return str(notifyData[0]["name"] + "涨幅为:" + notifyData[0]["increase"] + "%")
 
 
 if __name__ == '__main__':
-    get_stock_index()
-    add_SW_increase()
+    get_index_increase()
+    get_fund_increase()
+    add_sw_increase()
     notify_with_markdown()
