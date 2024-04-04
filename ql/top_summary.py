12f0c21ce10b8d27d9306e462c72d5bc3f11d5ac
mgmg22 <d33hcx@qq.com>
2024-04-04 19:50:57 +0800

    add:微博热搜列表notify

--- ql/top_summary.py
+++ ql/top_summary.py
@@ -6,10 +6,11 @@
 # @Desc  : 微博热搜列表
 from bs4 import BeautifulSoup
 import requests
+import notify
 
 verify = False
 
-summary = []
+summary_list = []
 
 
 # 定义一个过滤函数，用于移除包含特定字符串的tr元素
@@ -23,37 +24,57 @@ def filter_tr(tr):
     td_text3 = tr.select('td.td-03')
     text = td_text2[0].find('a').get_text()
     num = td_text1[0].get_text()
-    # td_text = tr.find('td').get_text()
-    # print(str(td_text1[0]))
-    # print(num+text)
-    # print(text)
-    print(td_text3[0])
+    href = td_text2[0].find('a')['href']
+    # print(str(td_text2[0]))
+    state = td_text3[0].get_text()
+    # 排除项
+    conditions = [
+        "剧集" in str(td_text2[0]),
+        "综艺" in str(td_text2[0]),
+        "电影" in str(td_text2[0]),
+        # todo emoji
+        # "[舔屏]" in str(td_text2[0])
+    ]
     # 过滤置顶
     if "icon-top" in str(td_text1[0]):
         return False
-    if "剧集" in str(td_text2[0]):
+    if any(conditions):
         return False
     # todo 过滤广告
-    return num + "." + text
+    item = {
+        'title': num + "." + text,
+        'href': href,
+        'state': state,
+    }
+    summary_list.append(item)
 
 
 def get_top_summary():
     url = 'https://s.weibo.com/top/summary'
     # todo 环境变量
     headers = {
-        'Cookie': "SUB=_2AkMRUtBSf8NxqwFRmfsUxGrkbop-wg7EieKnDiGJJRMxHRl-yT9kql1ZtRB6OtL-vTbTNhcLy7AgHY2b5GT7UADcvUnR; SUBP=0033WrSXqPxfM72-Ws9jqgMF55529P9D9W5B4Q3QrW71r.NcWldvRS-8; _s_tentry=passport.weibo.com; Apache=4426240446675.045.1712217957934;",
+        'Cookie': "SUB=_2AkMRUtBSf8NxqwFRmfsUxGrkbop-wg7EieKnDiGJJRMxHRl-yT9kql1ZtRB6OtL-vTbTNhcLy7AgHY2b5GT7UADcvUnR;"
     }
     data = requests.get(url, headers=headers, verify=verify)
     data.encoding = 'utf-8'
     soup = BeautifulSoup(data.text, 'html.parser')
     tr_elements = soup.select('#pl_top_realtimehot > table > tbody> tr')
+    for tr in tr_elements:
+        filter_tr(tr)
 
-    filtered_values = [filter_tr(tr) for tr in tr_elements if filter_tr(tr)]
 
-    # todo notify
-    print(filtered_values)
+def notify_markdown():
+    content = '''# 微博热搜'''
+    for item in summary_list:
+        state_mark = f'【{item["state"]}】' if item['state'] else ''
+        content += f'''
+[{item['title']}]({item['href']}){state_mark}
+'''
+    notify.serverJMy(summary_list[0]["title"], content)
+    with open("summary.md", 'w', encoding='utf-8') as f:
+        f.write(content)
 
 
 if __name__ == '__main__':
     get_top_summary()
-    # todo notify
+    notify_markdown()
